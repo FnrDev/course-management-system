f38ab7dfde739acce1a74c5117630dfe5661e022
Ahmed <fnr5541@gmail.com>
2026-07-16 10:14:36 +0300

    create course schema

---
+++ models/Course.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+
+const courseSchema = new mongoose.Schema({
+    code: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true,
+        minLength: 3,
+        maxLength: 100
+    },
+    description: {
+        type: String,
+        required: true,
+        minLength: 3,
+        maxLength: 100
+    },
+    credits: {
+        type: Number,
+        required: true
+    },
+    capacity: {
+        type: Number,
+        required: true
+    },
+    enrolledCount: {
+        type: Number
+    },
+    instructor: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Instructor"
+    },
+    is_active: {
+        type: Boolean,
+        default: true
+    },
+}, { timestamps: true })
+
+const Course = mongoose.model('Course', courseSchema)
+
+module.exports = Course
