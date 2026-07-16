18dd55e2e0162847b36d63d95bc9360387ab19af
Ahmed <fnr5541@gmail.com>
2026-07-16 14:31:33 +0300

    checkRole for instructors controller

--- controllers/instructors.controllers.js
+++ controllers/instructors.controllers.js
@@ -1,3 +1,4 @@
+const checkRole = require('../middleware/checkRole')
 const isSignedIn = require('../middleware/is-signed-in')
 const Instructor = require('../models/Instructor')
 const User = require('../models/user')
@@ -10,14 +11,14 @@ router.get('/', async (req, res) => {
 })
 
 // New instructor form (admin)
-router.get('/new', async (req, res) => {
+router.get('/new', isSignedIn, checkRole("admin"), async (req, res) => {
     // show list of all users to display them in UI ti make them instructors
     const users = await User.find()
     res.render('instructors/create-instructors.ejs', { users })
 })
 
 // Create instructor (admin)
-router.post('/', async (req, res) => {
+router.post('/', isSignedIn, checkRole("admin"), async (req, res) => {
     const {
         staffNumber,
         firstName,
@@ -44,13 +45,13 @@ router.get('/:id', async (req, res) => {
 })
 
 // Edit instructor form (admin)
-router.get('/:id/edit', async (req, res) => {
+router.get('/:id/edit', isSignedIn, checkRole("admin"), async (req, res) => {
     const instructor = Instructor.findById(req.params.id)
     res.render('instructors/edit-instructors.ejs', { instructor })
 })
 
 // Update instructor (admin)
-router.put('/:id', async (req, res) => {
+router.put('/:id', isSignedIn, checkRole("admin"), async (req, res) => {
     const {
         firstName,
         lastName,
@@ -66,7 +67,8 @@ router.put('/:id', async (req, res) => {
     res.redirect('/instructors')
 })
 
-router.delete('/:id', async (req, res) => {
+// Delete instructor (admin)
+router.delete('/:id', isSignedIn, checkRole("admin"), async (req, res) => {
     await Instructor.findByIdAndUpdate(req.params.id, {
         status: "inactive"
     })
