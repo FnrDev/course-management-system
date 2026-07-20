47762df2385b4e78d79503992dc68d309d6c231a
Ahmed <fnr5541@gmail.com>
2026-07-20 12:30:49 +0300

    create student details page

--- controllers/students.controllers.js
+++ controllers/students.controllers.js
@@ -1,5 +1,6 @@
 const checkRole = require('../middleware/checkRole');
 const Student = require('../models/Student')
+const Enrollment = require('../models/Enrollment')
 
 const router = require('express').Router()
 
@@ -11,7 +12,8 @@ router.get('/', checkRole("admin"), async (req, res) => {
 
 router.get('/:id', async (req, res) => {
     const student = await Student.findById(req.params.id)
-    res.render('students/details-student.ejs', { student })
+    const courses = await Enrollment.find({ student: student._id, status: 'enrolled' }).populate('course')
+    res.render('students/details-student.ejs', { student, courses })
 })
 
 // I know we can edit any user just by changing id
